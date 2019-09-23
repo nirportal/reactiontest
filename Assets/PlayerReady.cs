29f1e1c740abdfe664976ab36b3ab691d5ddf3ab
nirportal <55716718+nirportal@users.noreply.github.com>
2019-09-23 23:44:40 +0300

    reaction

---
+++ Assets/PlayerReady.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerReady : MonoBehaviour
+{   
+    public Text timerText;
+    float startTime;
+    bool haspreseed = false;
+    bool test = false;
+    bool hasStarted = false;
+
+
+    void Update()
+    {
+        if (Input.anyKey && hasStarted == false)
+        {
+            timerText.text = "first test is about reaction time,\n try to press the left mouse butten when the backgroun turns red \n as fast as you can! \n press the 'g' key to start";
+            hasStarted = true;         
+        }
+        if (hasStarted = true && Input.GetKey("g"))
+        {
+            inTest();
+        }
+
+    }
+
+
+    void inTest()
+    {
+        if (haspreseed == false && hasStarted == true)
+        {
+            startTime = Time.time;
+            haspreseed = true;
+            test = true;
+        }
+        if (test)
+        {
+            setTimer();
+        }
+    }
+
+
+    void setTimer()
+    {
+        if (haspreseed)
+        {
+            float t = Time.time - startTime;
+            string seconds = (t % 60).ToString("f3");
+            timerText.text = seconds;
+        }
+    }
+}
